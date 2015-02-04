1f00dba1129c12f38fa51f964fa903b04ff94696
Ming <gmbody7@gmail.com>
2015-02-04 14:15:19 -0500

    Send all values from sensors.  Use Datamap to send name and data

--- mobile/src/main/java/com/example/mxu24/moto360sensortest/MainActivity.java
+++ mobile/src/main/java/com/example/mxu24/moto360sensortest/MainActivity.java
@@ -1,17 +1,62 @@
 package com.example.mxu24.moto360sensortest;
 
+import android.app.PendingIntent;
+import android.content.Intent;
+import android.net.Uri;
+import android.support.v4.app.NotificationCompat;
+import android.support.v4.app.NotificationManagerCompat;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.wearable.Asset;
+import com.google.android.gms.wearable.DataApi;
+import com.google.android.gms.wearable.DataEvent;
+import com.google.android.gms.wearable.DataEventBuffer;
+import com.google.android.gms.wearable.DataItemAsset;
+import com.google.android.gms.wearable.DataMap;
+import com.google.android.gms.wearable.DataMapItem;
+import com.google.android.gms.wearable.Wearable;
+
+import org.apache.http.HttpResponse;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.entity.StringEntity;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.IOException;
+import java.io.UnsupportedEncodingException;
+import java.util.ArrayList;
+import java.util.Set;
 
 
-public class MainActivity extends ActionBarActivity {
+public class MainActivity extends ActionBarActivity implements DataApi.DataListener, GoogleApiClient.ConnectionCallbacks, GoogleApiClient.OnConnectionFailedListener {
 
+    private final static String TAG = "Main";
+    private Button pushButton;
+    private GoogleApiClient mGoogleApiClient;
+
+    private ArrayList<String> sensorsList;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        pushButton = (Button)findViewById(R.id.pushButton);
+
+        mGoogleApiClient = new GoogleApiClient.Builder(this).
+                addApi(Wearable.API)
+                .addConnectionCallbacks(this)
+                .addOnConnectionFailedListener(this)
+                .build();
+        init();
+
     }
 
 
@@ -35,5 +80,150 @@ public class MainActivity extends ActionBarActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    protected void onResume(){
+        super.onStart();
+        mGoogleApiClient.connect();
+    }
+    @Override
+    protected void onStart() {
+        super.onStart();
+    }
+
+
+    @Override
+    public void onPause(){
+        super.onPause();
+        Wearable.DataApi.removeListener(mGoogleApiClient,this);
+        mGoogleApiClient.disconnect();
+    }
+
+    @Override
+    protected void onStop(){
+        if (null != mGoogleApiClient && mGoogleApiClient.isConnected()) {
+            Wearable.DataApi.removeListener(mGoogleApiClient, this);
+            mGoogleApiClient.disconnect();
+        }
+        super.onStop();
+    }
+    @Override
+    public void onConnected(Bundle bundle) {
+        if (Log.isLoggable(TAG, Log.DEBUG)) {
+            Log.d(TAG, "Connected to Google Api Service");
+        }
+        Wearable.DataApi.addListener(mGoogleApiClient, this);
+    }
+
+    @Override
+    public void onConnectionSuspended(int i) {
+
+    }
+
+    @Override
+    public void onConnectionFailed(ConnectionResult connectionResult) {
+
+    }
+
+    private void init()
+    {
+        pushButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                int notificationId = 001;
+                Intent viewIntent = new Intent(MainActivity.this, NotificationActivity.class);
+                //viewIntent.putExtra(EXTRA_EVENT_ID, eventId);
+                PendingIntent viewPendingIntent =
+                        PendingIntent.getActivity(MainActivity.this, 0, viewIntent, 0);
+
+                NotificationCompat.Builder notificationBuilder =
+                        new NotificationCompat.Builder(MainActivity.this)
+                                .setSmallIcon(R.drawable.ic_event)
+                                .setContentTitle("1")
+                                .setContentText("2")
+                                .setContentIntent(viewPendingIntent);
+
+// Get an instance of the NotificationManager service
+                NotificationManagerCompat notificationManager =
+                        NotificationManagerCompat.from(MainActivity.this);
+
+// Build the notification and issues it with notification manager.
+                notificationManager.notify(notificationId, notificationBuilder.build());
+            }
+        });
+    }
+
+    @Override
+    public void onDataChanged(DataEventBuffer dataEvents) {
+        for (DataEvent event : dataEvents) {
+            if (event.getType() == DataEvent.TYPE_DELETED) {
+                Log.d(TAG, "DataItem deleted: " + event.getDataItem().getUri());
+                //event.getDataItem().getAssets().get("1");
+            } else if (event.getType() == DataEvent.TYPE_CHANGED) {
+
+                Log.d(TAG, "DataItem changed: " + event.getDataItem().getUri());
+                Uri dataItemUri = event.getDataItem().getUri();
+
+                DataMap dataMap = DataMapItem.fromDataItem(event.getDataItem())
+                        .getDataMap();
+
+                Set sensorKeys  = dataMap.keySet();
+
+                //need some more check
+                String sensorName = (String)sensorKeys.iterator().next();
+                float[] values = dataMap.getFloatArray(sensorName);
+
+                Log.i(sensorName, Float.toString(values[0]));
+                /*if(sensorsList == null){
+                    sensorsList = dataMap.getStringArrayList("SensorsList");
+                }
+                else{
+                    createDataStream(dataMap);
+                }*/
+
+                /*float acceleration = dataMap.getFloat("Accelerometer");
+                Log.i(TAG, String.valueOf(acceleration));
+                JSONObject jsonObj = new JSONObject();
+                try {
+                    jsonObj.put("Accelerometer",acceleration);
+
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+                DefaultHttpClient httpclient = new DefaultHttpClient();
+                HttpPost httppostreq = new HttpPost("https://dweet.io/dweet/for/wearable");
+                try {
+                    StringEntity se = new StringEntity(jsonObj.toString());
+                    httppostreq.setEntity(se);
+                    httppostreq.setHeader("Accept", "application/json");
+                    httppostreq.setHeader("Content-type", "application/json");
+                    try {
+                        HttpResponse httpresponse = httpclient.execute(httppostreq);
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                } catch (UnsupportedEncodingException e) {
+                    e.printStackTrace();
+                }*/
+
+
+
+
+            }
+        }
+    }
+
+    private float[] createDataStream(DataMap dataMap) {
+        float[] values = null;
+        for(String sensorType:sensorsList){
+
+            values = dataMap.getFloatArray(sensorType);
+            if(values!=null) {
+                Log.i(sensorType,Float.toString(values[0]));
+                return values;
+            }
+        }
+        return values;
     }
 }
--- wear/src/main/java/com/example/mxu24/moto360sensortest/MainActivity.java
+++ wear/src/main/java/com/example/mxu24/moto360sensortest/MainActivity.java
@@ -11,16 +11,31 @@ import android.support.wearable.view.WatchViewStub;
 import android.util.Log;
 import android.widget.TextView;
 
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.common.api.PendingResult;
+import com.google.android.gms.wearable.DataApi;
+import com.google.android.gms.wearable.PutDataMapRequest;
+import com.google.android.gms.wearable.PutDataRequest;
+import com.google.android.gms.wearable.Wearable;
+
+import java.util.ArrayList;
 import java.util.List;
 
-public class MainActivity extends Activity implements SensorEventListener {
+public class MainActivity extends Activity implements
+        SensorEventListener,
+        GoogleApiClient.ConnectionCallbacks,
+        GoogleApiClient.OnConnectionFailedListener{
+
+
+    private GoogleApiClient mGoogleApiClient;
 
     private TextView mTextView;
     private SensorManager mSensorManager;
     private static final String TAG = "Moto360SensorTest";
     private Sensor mSensor;
     private List<Sensor> deviceSensors;
-
+    private ArrayList<String> sensorsList;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -29,65 +44,52 @@ public class MainActivity extends Activity implements SensorEventListener {
         stub.setOnLayoutInflatedListener(new WatchViewStub.OnLayoutInflatedListener() {
             @Override
             public void onLayoutInflated(WatchViewStub stub) {
+
+                //the content in stub has to be set in the callback
                 mTextView = (TextView) stub.findViewById(R.id.text);
+                mTextView.setText("Sensor Data Transferring");
             }
         });
         mSensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
         deviceSensors = mSensorManager.getSensorList(Sensor.TYPE_ALL);
         for(Sensor s : deviceSensors){
             Log.i(TAG, "" + s.getStringType());
-
+            //sensorsList.add(s.getStringType());
         }
-        int count = deviceSensors.size();
-        //mTextView.setText(count);
+
+        mGoogleApiClient = new GoogleApiClient.Builder(this)
+                .addApi(Wearable.API)
+                .addConnectionCallbacks(this)
+                .addOnConnectionFailedListener(this)
+                .build();
+        mGoogleApiClient.connect();
     }
 
     @Override
     public void onSensorChanged(SensorEvent event) {
-        /*if (event.sensor.getType() == Sensor.TYPE_ACCELEROMETER) {
-            getAccelerometer(event);
-        }*/
-        Log.i(event.sensor.getStringType(),Float.toString(event.values[0]));
-        //switch(event.sensor.getType()){
-            /*case Sensor.TYPE_ACCELEROMETER:{
-                getAccelerometer(event);
-                break;
-            }
-            case Sensor.TYPE_STEP_COUNTER:{
-                Log.i("Step Counter", Float.toString(event.values[0]));
-                break;
-            }
-            case Sensor.TYPE_GYROSCOPE :{
-                Log.i("Gesture", Float.toString(event.values[0]));
-                break;
-            }
-            case Sensor.TYPE_MAGNETIC_FIELD :{
-                Log.i("Magnetic", Float.toString(event.values[0]));
-                break;
-            }
-            case Sensor.TYPE_LIGHT :{
-                Log.i("Light", Float.toString(event.values[0]));
-                break;
-            }
-            case Sensor.TYPE_HEART_RATE :{
-                Log.i("Gesture", Float.toString(event.values[0]));
-                break;
-            }
-        }*/
 
+            Log.i(event.sensor.getStringType(),Float.toString(event.values[0]));
 
+            String path = "/location"+"/"+ System.currentTimeMillis();
+            PutDataMapRequest putDataMapRequest = PutDataMapRequest.create(path);
+            putDataMapRequest.getDataMap().putFloatArray(event.sensor.getStringType(),event.values);
+            //putDataMapRequest.getDataMap().putFloat("Accelerometer", getAccelerometer(event));
+            PutDataRequest request = putDataMapRequest.asPutDataRequest();
+            PendingResult<DataApi.DataItemResult> pendingResult = Wearable.DataApi
+            .putDataItem(mGoogleApiClient, request);
     }
 
-    private void getAccelerometer(SensorEvent event) {
+    private float getAccelerometer(SensorEvent event) {
         float[] values = event.values;
         // Movement
         float x = values[0];
         float y = values[1];
         float z = values[2];
 
-        float accelationSquareRoot = (x * x + y * y + z * z)
+        float accelerationSquareRoot = (x * x + y * y + z * z)
                 / (SensorManager.GRAVITY_EARTH * SensorManager.GRAVITY_EARTH);
-        Log.i("Acceleration", Float.toString(accelationSquareRoot));
+        Log.i("Acceleration", Float.toString(accelerationSquareRoot));
+        return(accelerationSquareRoot);
     }
 
     private void getStepCounter(SensorEvent event) {
@@ -97,9 +99,6 @@ public class MainActivity extends Activity implements SensorEventListener {
         float y = values[1];
         float z = values[2];
 
-        float accelationSquareRoot = (x * x + y * y + z * z)
-                / (SensorManager.GRAVITY_EARTH * SensorManager.GRAVITY_EARTH);
-        Log.i(TAG, Float.toString(accelationSquareRoot));
     }
 
     @Override
@@ -129,5 +128,27 @@ public class MainActivity extends Activity implements SensorEventListener {
         // unregister listener
         super.onPause();
         mSensorManager.unregisterListener(this);
+    }
+
+    @Override
+    public void onConnected(Bundle bundle) {
+
+
+        PutDataMapRequest putDataMapReq = PutDataMapRequest.create("/SensorList");
+
+        putDataMapReq.getDataMap().putStringArrayList("SensorsList",sensorsList);
+        PutDataRequest putDataReq = putDataMapReq.asPutDataRequest();
+        PendingResult<DataApi.DataItemResult> pendingResult =
+                Wearable.DataApi.putDataItem(mGoogleApiClient, putDataReq);
+    }
+
+    @Override
+    public void onConnectionSuspended(int i) {
+
+    }
+
+    @Override
+    public void onConnectionFailed(ConnectionResult connectionResult) {
+
     }
 }
